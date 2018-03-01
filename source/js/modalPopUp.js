7105f5bb61083745c663daa45a19c5ee230a95ac
Krasavin Serg <krasavin.serg@gmail.com>
2018-03-01 13:02:19 +0300

    правки

---
+++ source/js/modalPopUp.js
@@ -0,0 +1,22 @@
+var order_btn = document.querySelector (".best-goods__button-order");
+// var cart = document.querySelector (".test");
+var popup = document.querySelector(".modal");
+
+
+order_btn.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  popup.classList.toggle("modal-show");
+});
+
+// cart.addEventListener("click", function (evt) {
+//   evt.preventDefault();
+//   popup.classList.toggle("modal-show");
+// });
+
+window.addEventListener("keydown", function (evt) {
+  if (evt.keyCode === 27) {
+    if (popup.classList.contains("modal-show")) {
+      popup.classList.remove("modal-show");
+    }
+  }
+});
